896445e94dc18fa145b6a93c873313932a15ed42
Kim Starr <98854861+kimbaqueen@users.noreply.github.com>
2022-04-14 22:28:12 -0600

    initial commit

---
+++ sldc.js
@@ -0,0 +1,27 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+let analysisOfRequirements = "This is the 2nd step, this process includes defining what they are building, how it should work, and how will it be used. The requirements are determined by client/business needs and how those needs are translated to the development team.";
+const design = "This is the 3rd step, user stories can be used to help determine how the product will be used or needs to be accessed, which also influences the design. Design team creates wireframes to help demonstrate to the developers key features they need to be created and usually include the desired layout/style.";
+let implementation = "This is the 4th step, includes coding the product. Development team takes the information received and uses project management methodology (typically agile or waterfall) to create a strategic roadmap to complete the tasks needed.";
+const testingAndIntegration = "This is the 5th step, after the coding is completed it is released into a testing environment. All areas of an application are tested to spot any defect, bugs, or other problems. After testing is completed this becomes depoloyed into production.";
+let maintain = "This is the 6th step, after the product is live and being used, the focus shifts to maintainance. This may include enhancements, bug fixes, and adding new features.";
